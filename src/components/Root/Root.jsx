4f41e08aa43248e256ecd87e05773dc660c0e840
toymurrahman <toymurrahman2@gmail.com>
2025-02-01 12:36:02 +0600

    Added tommy

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 const Root = () => {
     return (
         <div className='max-w-6xl mx-auto'>
-            
+             <h1 className='text-9xl text-red-600'>Hello <span className='font-bold text-green-500'>Tonmoy</span> Kala pacha</h1>
             <Navbar/>
 
 
